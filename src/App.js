2d34820b5aa4a78946143a2f0db85851c75b1553
Harper Nettesheim <harpernettesheim@Harpers-MacBook-Pro.local>
2020-07-06 15:24:54 -0500

    Added functionality. Able to add items to the list and render to the page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,60 @@
 import React from 'react';
+import TodoList from './components/TodoList'
+import TodoForm from './components/TodoForm'
+
+const tasks =[
+  {
+    task: 'Organize Garage',
+    id: Date.now(),
+    completed: false
+  },
+  {
+    task: 'Bake Cookies',
+    id: Date.now(),
+    completed: false
+  }
+];
 
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+  constructor(){
+    super();
+    this.state = {
+      tasks
+    };
+  }
+
+  addTask = (e, item) => {
+    e.preventDefault();
+    const newTask = {
+      task: item,
+      id: Date.now(),
+      completed: false
+    };
+    this.setState({
+      tasks: [...this.state.tasks, newTask]
+    })
+
+  }
+
   render() {
     return (
       <div>
         <h2>Welcome to your Todo App!</h2>
-      </div>
+        <div>
+          <TodoList 
+            tasks = {this.state.tasks}
+          />
+        </div>
+        <div>
+          <TodoForm addTask={this.addTask}/>
+        </div>
+      </div> 
     );
   }
+  
 }
 
 export default App;
---
+++ src/components/TodoForm.js
@@ -0,0 +1,37 @@
+import React from 'react'
+
+class TodoForm extends React.Component {
+    constructor() {
+        super();
+        this.state ={
+            task: ''
+        }
+    }
+
+    submitTask = e => {
+        e.preventDefault();
+        this.setState({task: ''});
+        this.props.addTask(e, this.state.task);
+    }
+
+    changes = e => {
+        this.setState({ [e.target.name]: e.target.value})
+    }
+
+    render() {
+        return(
+            <form onSubmit={this.submitTask}>
+                <input 
+                    type="text"
+                    value={this.state.task}
+                    name="task"
+                    onChange={this.changes}
+                />
+                <button>Add</button>
+                <button>Clear Completed</button>
+            </form>
+        )
+    }
+}
+
+export default TodoForm;
